59cafe2a35421798f8d452203b4f77e889b15fe4
Budama87 <amb.amarasinghe@gmail.com>
2019-09-12 10:16:15 +0530

    Update Program.cs

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,52 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                int ansFive;
+                int ansThree;
+                int ansFiftee;
+
+                ansThree = i % 3;
+
+                if (ansThree == 0)
+                {
+                    ansFive = i % 5;
+                    if (ansFive == 0)
+                    {
+
+                        ansFiftee = i % 15;
+                        if (ansFiftee == 0)
+                        {
+
+                            Console.WriteLine(i + "Fizzbuzz");
+                        }
+                        else
+                        {
+
+
+                            Console.WriteLine(i);
+                        }
+
+
+
+                        Console.WriteLine(i + "buzz");
+                    }
+                    else
+                    {
+
+
+                        Console.WriteLine(i);
+                    }
+
+
+                    Console.WriteLine(i + "Fizz");
+                }
+
+               
+
+            }
+            Console.ReadLine();
         }
     }
 }
